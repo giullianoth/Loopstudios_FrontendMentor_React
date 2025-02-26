a411257cae95b649f569cfc40d6ab57bf03ada19
Giulliano <tharsoweb@gmail.com>
2025-02-26 11:36:26 -0300

    headline section

--- src/components/page/Headline/index.jsx
+++ src/components/page/Headline/index.jsx
@@ -1,8 +1,21 @@
+import Container from "../../common/Container"
 import styles from "./Headline.module.css"
 
 const Headline = () => {
   return (
-    <div>Headline</div>
+    <section className={styles.headline}>
+      <Container className={styles.headline__container}>
+        <div className={styles.headline__wrapper}>
+          <header className={styles.headline__title}>
+            <h1>
+              <span>Immersive</span>
+              <span>experiences</span>
+              <span>that deliver</span>
+            </h1>
+          </header>
+        </div>
+      </Container>
+    </section>
   )
 }
 
