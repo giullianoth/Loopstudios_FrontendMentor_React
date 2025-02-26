3a9e37ffc641900261bc6b18b41051940709b46a
Giulliano <tharsoweb@gmail.com>
2025-02-26 11:40:22 -0300

    presentation section

--- src/components/page/Presentation/index.jsx
+++ src/components/page/Presentation/index.jsx
@@ -1,8 +1,32 @@
+import Container from "../../common/Container"
 import styles from "./Presentation.module.css"
+import mobilePresentationImage from "../../../assets/images/mobile/image-interactive.jpg"
+import desktopPresentationImage from "../../../assets/images/desktop/image-interactive.jpg"
 
 const Presentation = () => {
   return (
-    <div>Presentation</div>
+    <section className={styles.presentation}>
+      <Container spaced={true}>
+        <div className={styles.presentation__wrapper}>
+          <div className={styles.presentation__image}>
+            <picture>
+              <source media="(min-width: 1024px)" srcSet={desktopPresentationImage} />
+              <img src={mobilePresentationImage} alt="Interactive VR" />
+            </picture>
+          </div>
+
+          <div className={styles.presentation__info}>
+            <header className={styles.presentation__title}>
+              <h1>The leader in interactive VR</h1>
+            </header>
+
+            <p className={styles.presentation__text}>Founded in 2011, Loopstudios has been producing world-class virtual reality
+              projects for some of the best companies around the globe. Our award-winning
+              creations have transformed businesses through digital experiences that bind</p>
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
